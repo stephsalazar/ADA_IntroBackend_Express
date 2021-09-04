c9983357379b0f6048c1bf095c38a24d6d20d092
stephSalazar <anastephany.salazarguzman@gmail.com>
2021-09-04 16:05:05 -0500

    Add: BodyParser dependency

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const port = 3030;
 
+
+const arrayPerritos = [
+  {
+    raza: 'pitbull',
+    color: 'gris',
+    edad: 'cachorro',
+  },
+  {
+    raza: 'chihuahua',
+    color: 'miel',
+    edad: 'adulto',
+  },
+  {
+    raza: 'labrador',
+    color: 'negro',
+    edad: 'joven',
+  },
+  {
+    raza: 'bulldog',
+    color: 'blanco&cafe',
+    edad: 'adulto',
+  },
+]
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+
+// Ruta raíz (Aqui inicia nuestro servidor)
 app.get('/', (req, res) => {
-  res.send('Gatitos');
+  res.send('Gatitos y perros');
 });
 
+// Ruta básica & retorna archivo estático 
 app.get('/mostrarGatito', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Ruta con parámetros & retorna texto simple
+app.get('/mostrarGatito/:raza', (req, res) => {
+  const raza = req.params.raza;
+  
+  console.log(raza);
+  res.send(`Foto de gatito tipo: ${raza}`);
+});
+
+// Ruta básica & retorna un JSON
 app.get('/fotosGatitos', function(req, res) {
   res.json({
     "big" : ["http://i.imgur.com/CRSzQ4r.png", "http://i.imgur.com/l94Rfsj.png", "http://i.imgur.com/304PJ9p.png"],
@@ -21,10 +64,17 @@ app.get('/fotosGatitos', function(req, res) {
   });
 });
 
+app.get('/perritos', (req, res) => {
+  res.json(arrayPerritos);
+});
+
+
+
+// Función para mostrar error al ingresar cualquier otra ruta que no exista
 app.use(function (req, res, next) {
   res.status(404).send("Oh oh, parece que ingresaste un ruta equivocada")
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
--- saludar.js
+++ saludar.js
@@ -1,1 +1,17 @@
 console.log("Hola, este es mi primer script desde archivo JS");
+
+const suma = (num) => {
+  return num + num
+};
+
+const multiplicacion = (suma) => {
+  return suma * 2
+};
+
+suma(5); // 10
+const diez = suma(5);
+console.log(diez) // 10
+
+multiplicacion(suma(2));
+
+console.log('Callback', multiplicacion(suma(2)));
